0e199ba4981d9b9998ff302a5b0a1daec016673f
Sabbirz <sabbir.kuasha@gmail.com>
2025-11-12 13:15:05 +0600

    Update b_event.js

--- evento/evento/doctype/b_event/b_event.js
+++ evento/evento/doctype/b_event/b_event.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2025, sabbir.kuasha@gmail.com and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("B Event", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("B Event", {
+	refresh(frm) {
+		console.log(frm);
+	},
+});
