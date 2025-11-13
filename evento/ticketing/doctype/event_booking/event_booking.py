e5bcae3df550e1101b8b761fd0610c88569b8844
sabbirz <sabbir.kuasha@gmail.com>
2025-11-14 02:03:12 +0600

    feat: Type Checking added & Event booking total calculated
    Type Checking added
    Event booking total calculated

--- evento/ticketing/doctype/event_booking/event_booking.py
+++ evento/ticketing/doctype/event_booking/event_booking.py
@@ -6,4 +6,33 @@ from frappe.model.document import Document
 
 
 class EventBooking(Document):
-	pass
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from evento.ticketing.doctype.event_booking_attendee.event_booking_attendee import EventBookingAttendee
+		from frappe.types import DF
+
+		amended_from: DF.Link | None
+		currency: DF.Link | None
+		event: DF.Link
+		event_attendees: DF.Table[EventBookingAttendee]
+		name: DF.Int | None
+		total_amount: DF.Currency
+		user: DF.Link
+	# end: auto-generated types
+
+	def validate(self):
+		self.set_total()
+		self.set_currency()
+
+	def set_total(self):
+		self.total_amount = 0
+		for attendee in self.event_attendees:
+			self.total_amount += attendee.price
+		
+		
+	def set_currency(self):
+		self.currency = self.event_attendees[0].currency
--- evento/ticketing/doctype/event_booking_attendee/event_booking_attendee.py
+++ evento/ticketing/doctype/event_booking_attendee/event_booking_attendee.py
@@ -6,4 +6,22 @@ from frappe.model.document import Document
 
 
 class EventBookingAttendee(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		currency: DF.Link
+		email: DF.Data
+		full_name: DF.Data
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		price: DF.Currency
+		ticket_type: DF.Link
+	# end: auto-generated types
+
 	pass
