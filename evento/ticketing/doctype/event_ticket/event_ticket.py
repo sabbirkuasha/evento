82fceb6f9f1886aa774e1b92bbef2c75220392cd
Sabbirz <sabbir.kuasha@gmail.com>
2025-11-19 11:10:48 +0600

    qr code generated for every ticket

--- evento/ticketing/doctype/event_ticket/event_ticket.py
+++ evento/ticketing/doctype/event_ticket/event_ticket.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2025, sabbir.kuasha@gmail.com and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
@@ -18,7 +18,31 @@ class EventTicket(Document):
 		attendee_name: DF.Data
 		booking: DF.Link
 		event: DF.Link
+		qr_code: DF.AttachImage | None
 		ticket_type: DF.Link
 	# end: auto-generated types
 
-	pass
+	
+	def after_insert(self):
+		self.generate_qr_code()
+	
+	def generate_qr_code(self):
+		import qrcode
+		import io
+
+		img = qrcode.make(self.name)
+		img_bytes = io.BytesIO()
+		img.save(img_bytes, format='PNG')
+		file_content = img_bytes.getvalue()
+
+		qr_code_file = frappe.get_doc({
+			"doctype": "File",
+			"content": file_content,
+			"attached_to_doctype": "Event Ticket",
+			"attached_to_name": self.name,
+			"attached_to_field": "qr_code",
+			"file_name": f"ticket-qr-code-{self.name}.png"
+		}).save()
+
+		self.qr_code = qr_code_file.file_url
+		self.save()
